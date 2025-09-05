bb3f9d8d5e127681e189ecc2e4aa652afa0012df
tremorscript <tremorscipt@gmail.com>
2025-09-05 13:32:45 +0530

    Added ContributorById Spec

---
+++ src/Acme.SampleToDo.Core/ContributorAggregate/Specifications/ContributorByIdSpec.cs
@@ -0,0 +1,10 @@
+﻿namespace Acme.SampleToDo.Core.ContributorAggregate.Specifications;
+
+public class ContributorByIdSpec : Specification<Contributor>
+{
+  public ContributorByIdSpec(int contributorId)
+  {
+    Query
+      .Where(contributor => contributor.Id == contributorId);
+  }
+}
